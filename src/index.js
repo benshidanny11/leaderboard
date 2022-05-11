834fa30d9d4d97d2fefe90725a184ae7974b27aa
benshidanny11 <benshidanny11@gmail.com>
2022-05-11 22:15:36 +0200

    Implement hit on api functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+/* eslint-disable no-restricted-globals */
 import './styles/style.css';
+import Game from './modules/feature.js';
+
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+
+document.getElementById('refButton').addEventListener('click', () => {
+  location.reload();
+});
+
+document.addEventListener('DOMContentLoaded', async () => {
+  if (!localStorage.getItem('mGameId')) {
+    await Game.createGame(url);
+  }
+  const scores = await Game.fetchScores(`${url}/${Game.getGameID()}/scores`);
+  await Game.addScrore(`${url}/${Game.getGameID()}/scores`);
+  Game.populateData(scores);
+});
